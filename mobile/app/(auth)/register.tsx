476a78fd4b4b2e99ae1069243ff6c7bbfa74d16c
AnzeFric <anzeifric@gmail.com>
2025-03-17 16:07:36 +0100

    Prepared styled screens for login and register

--- mobile/app/(auth)/register.tsx
+++ mobile/app/(auth)/register.tsx
@@ -1,17 +1,156 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 import { router } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function RegisterScreen() {
+export default function LoginScreen() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [securePassword, setSecurePassword] = useState(true);
+
+  const handlePress = () => {
+    console.log("Register pressed");
+  };
+
   return (
-    <View>
-      <Text>Register</Text>
-      <Text
-        onPress={() => {
-          router.push("/(auth)/login");
-        }}
-      >
-        Already registered?
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Improve!</Text>
+      <Text style={styles.subtitle}>Embark on a new chapter in your life.</Text>
+
+      <View style={styles.contentContainer}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+            value={username}
+            onChangeText={setUsername}
+            autoCapitalize={"words"}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry={securePassword}
+            autoCapitalize="none"
+          />
+          <TouchableOpacity onPress={() => setSecurePassword(!securePassword)}>
+            <Ionicons
+              name={securePassword ? "eye-off" : "eye"}
+              size={20}
+              color="#888"
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Confirm password"
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+            secureTextEntry={true}
+            autoCapitalize="none"
+          />
+        </View>
+      </View>
+
+      <TouchableOpacity style={styles.button} onPress={handlePress}>
+        <Text style={styles.buttonText}>Register</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.link} onPress={() => router.push("/(auth)/login")}>
+        Already a member? Login
       </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#333",
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "#666",
+    marginBottom: 30,
+  },
+  contentContainer: {
+    width: "100%",
+    gap: 15,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    elevation: 3,
+    justifyContent: "space-between",
+    paddingRight: 20,
+  },
+  input: {
+    backgroundColor: "#fff",
+    padding: 15,
+    borderRadius: 10,
+    fontSize: 16,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    elevation: 3,
+    flex: 1,
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: "#4a90e2",
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderRadius: 10,
+    shadowColor: "#4a90e2",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    elevation: 5,
+    alignSelf: "flex-end",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  link: {
+    marginTop: 50,
+    color: "#4a90e2",
+    fontSize: 16,
+    textDecorationLine: "underline",
+  },
+});

c7b201d33e3fa99abce194a7ac32755e35f26495
AnzeFric <anzeifric@gmail.com>
2025-03-18 09:44:28 +0100

    Changed backend from Maven to Gradle Groovy and updated file structure

---
+++ backend/improve/src/main/java/com/example/anzefric/improve/improve/ImproveApplication.java
@@ -0,0 +1,15 @@
+package com.example.anzefric.improve.improve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImproveApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Starting Improve backend");
+        SpringApplication.run(ImproveApplication.class, args);
+        System.out.println("Improve backend started successfully!");
+    }
+}
+

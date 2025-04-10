2c7c0d65d2c2c76cd3c72c46f520f366fcd680a5
AnzeFric <anzeifric@gmail.com>
2025-04-10 13:19:46 +0200

    Backend: updated workout controller

--- backend/improve/src/main/java/com/anzefric/improve/controller/WorkoutController.java
+++ backend/improve/src/main/java/com/anzefric/improve/controller/WorkoutController.java
@@ -2,13 +2,17 @@ package com.anzefric.improve.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.anzefric.improve.data.model.user.User;
 import com.anzefric.improve.data.model.workout.Workout;
+import com.anzefric.improve.data.response.ApiResponse;
+import com.anzefric.improve.data.response.ApiResponseException;
 import com.anzefric.improve.service.WorkoutService;
 
 @RestController
@@ -19,12 +23,23 @@ public class WorkoutController {
     private WorkoutService workoutService;
 
     @PostMapping("/create")
-    public ResponseEntity<Workout> create(@RequestBody Workout workout) {
+    public ApiResponse<String> create(@RequestBody Workout workout) {
         try {
-            Workout createdWorkout = workoutService.create(workout);
-            return ResponseEntity.ok(createdWorkout);
+            User authenticatedUser = getCurrentAuthenticatedUser();
+            workout.setUserUuid(authenticatedUser.getUserUuid());
+            workoutService.create(workout);
+
+            return ApiResponse.success("Workout created successfully!");
         } catch (RuntimeException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+            throw new ApiResponseException(HttpStatus.BAD_REQUEST, "Error saving workout: " + e.getMessage());
         }
+    }
+
+    private User getCurrentAuthenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new ApiResponseException(HttpStatus.FORBIDDEN, "User not authenticated.");
+        }
+        return (User) authentication.getPrincipal();
     }
 }

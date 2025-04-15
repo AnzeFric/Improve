a1376433887e3b5992500c9406fd289a87b7dcf1
AnzeFric <anzeifric@gmail.com>
2025-04-15 14:26:08 +0200

    Backend, user service: removed try catch

--- backend/improve/src/main/java/com/anzefric/improve/service/UserService.java
+++ backend/improve/src/main/java/com/anzefric/improve/service/UserService.java
@@ -1,11 +1,9 @@
 package com.anzefric.improve.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.anzefric.improve.data.model.user.User;
-import com.anzefric.improve.data.response.ApiResponseException;
 import com.anzefric.improve.repository.UserRepository;
 
 @Service
@@ -15,11 +13,7 @@ public class UserService {
     private UserRepository userRepository;
 
     public void deleteUserByEmail(String userEmail) {
-        try {
-            User foundUser = userRepository.findByEmailIgnoreCase(userEmail);
-            userRepository.delete(foundUser);
-        } catch(Exception e) {
-            throw new ApiResponseException(HttpStatus.NOT_FOUND, "User not found.");
-        }
+        User foundUser = userRepository.findByEmailIgnoreCase(userEmail);
+        userRepository.delete(foundUser);
     }
 }

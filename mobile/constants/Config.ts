8a99a87a89ab0428c99bef02c15bc7959a319a4e
AnzeFric <anzeifric@gmail.com>
2025-05-14 21:32:31 +0200

    Frontend: Cleanup

--- mobile/constants/Config.ts
+++ mobile/constants/Config.ts
@@ -1,4 +1,3 @@
 import Config from "react-native-config";
 
 export const API_BASE_URL = `http://${Config.API_DEVELOPMENT_IP}:${Config.API_PORT}/api`;
-export const OPENAI_API_KEY = Config.OPENAI_API_KEY;
